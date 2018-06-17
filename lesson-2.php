8aa85c3fafd434932762852dbefcfa9975d06ad5
stanislavfor <stasposter@narod.ru>
2018-06-18 01:36:22 +0300

    added lesson-2 commit

--- lesson-2.php
+++ lesson-2.php
@@ -10,5 +10,39 @@
 <a href="lesson-3.php">PHP - 3. Основы PHP. Ассоциативные массивы.</a><br>
 <a href="lesson-4.php">PHP - 4. Основы PHP. Функции.</a><br>
 <a href="lesson-5.php">PHP - 5. Post и Get запросы.</a>
+
+<?php 
+
+
+$strings = array(
+"The Elder Scrolls 4",
+"Half Life 2",
+"FlatOut 2",
+"Unreal Tournament 3",
+"Warp",
+"Shank",
+"Bad Day LA",
+"The Walking Dead: Season Two",
+"GRID Autosport",
+"Valiant Hearts",
+"Star Conflict",
+"Path of Exile",
+"The Sims 2",
+"DOOM 3",
+"Война и мир"
+);
+
+$number = count($strings);
+
+print "<p>Лёгкие игры для ПК:</p><br>";
+
+echo "<ol>";
+for ($i=0; $i < $number; $i=$i+1) {
+		echo "<li> $strings[$i] </li>";
+}
+echo "</ol>";
+
+?>
+
 </body>
 </html>
