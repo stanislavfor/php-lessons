8ee796ea1c837a19e3bc7efd5597a917032a7b6c
stanislavfor <stasposter@narod.ru>
2018-06-18 01:32:04 +0300

    first commit

---
+++ lesson-2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>php-lessons</title>
+</head>
+<body>
+	<h1>PHP - 2. Основы PHP. Массивы.</h1>
+<a href="index.php">PHP - Главная страница</a><br><br>	
+<a href="lesson-3.php">PHP - 3. Основы PHP. Ассоциативные массивы.</a><br>
+<a href="lesson-4.php">PHP - 4. Основы PHP. Функции.</a><br>
+<a href="lesson-5.php">PHP - 5. Post и Get запросы.</a>
+</body>
+</html>
