d9d98cba27e9d9f9245db2e8f358fb5b984d929c
stanislavfor <stasposter@narod.ru>
2018-06-18 05:00:56 +0300

    added lesson-5 commit

--- lesson-5.php
+++ lesson-5.php
@@ -10,5 +10,37 @@
 <a href="lesson-2.php">PHP - 2. Основы PHP. Массивы.</a><br>
 <a href="lesson-3.php">PHP - 3. Основы PHP. Ассоциативные массивы.</a><br>
 <a href="lesson-4.php">PHP - 4. Основы PHP. Функции.</a><br>
+
+<h3>Форма отправки почтового сообщения:</h3><br>
+
+<?php
+
+
+if (!empty($_POST)) {
+	$message = "Вам новое сообщение с сайта. \n" 
+							. "Имя отправителя: " . $_POST['name'] . "\n"
+							. "Email отправителя: " .  $_POST['email'] . "\n"
+							. "Сообщение: \n" . $_POST['message'];
+
+	$result = mail("stasposter@narod.ru", "Message from page of PHP lesson", $message);
+
+	if ($result) {
+			echo "Ваше сообщение отправлено успешно!";
+	} else {
+			echo "Какая-то ошибка. Письмо не отправлено.";
+	}
+
+}
+
+?>
+
+<form action="" method="post">
+	<input type="text" name="name" placeholder="Укажите Ваше имя"><br><br>
+	<input type="text" name="email" placeholder="Укажите Ваш email"><br><br>
+	<textarea name="message" id="" cols="50" rows="8" placeholder="Напишите Ваше сообщение"></textarea><br>
+	<input type="reset" value="Сбросить/Отмена">
+	<input type="submit" value="Отправить сообщение">
+</form>
+
 </body>
 </html>
